89b3f1db0d85f472093688c42af8c09df255180c
mariafoo <maria.foo@polycademy.com>
2014-10-07 15:55:14 +1100

    Added comments

--- assign2_u5021276.py
+++ assign2_u5021276.py
@@ -69,20 +69,24 @@ def read_graph_from_file(filename):
                 # columns[0] => node1
                 # columns[1] => node2
                 
+                # check that the nodes are between 0 and N-1 where N is the number of nodes
                 if columns[0] < 0 or columns[0] > node_num or columns[1] < 0 or columns[1] > node_num:
                     raise FileFormatError(filename, line_num, "Incorrect link node number! Nodes must be between 0 and N-1 where N is the number of nodes.")
 
+                # create a key for a new node
                 if not columns[0] in neighbour_dict:
                     neighbour_dict[columns[0]] = []
                 
                 if not columns[1] in neighbour_dict:
                     neighbour_dict[columns[1]] = []
 
+                # add values to key 
                 neighbour_dict[columns[0]].append(columns[1])
                 neighbour_dict[columns[1]].append(columns[0])
 
         else:
 
+            # check that the file starts with nodes with 3 columns, then with links with 3 columns
             raise FileFormatError(filename, line_num, "Incorrect file format! File needs to start with nodes with 3 columns, then with links with 2 columns.")
 
     # close the file!
@@ -119,25 +123,47 @@ def label_graph_components(neighbour_list):
 
     # Depth First Search (adapted from Mann, E 2014, 'Depth-First Search and Breadth-First Search in Python', Eddmann blog, web log post, 5 March, viewed 6 October 2014, http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/)
     def depth_first_search(neigbour_list, root_node):
+
         visited_nodes = set()
         stack = [root_node]
+
+        # parse the stack
         while stack:
+
+            # get the element at the end of the list and return the element
             node = stack.pop()
+
+            # check if node is not in visited_nodes set
             if node not in visited_nodes:
+                # add node to visited_nodes set 
                 visited_nodes.add(node)
+                # find the unvisited neighbours of the node and add to the stack
                 stack.extend(set(neighbour_list[node]) - visited_nodes)
+
         return list(visited_nodes)
 
+    # subgraph label is from 0 to C-1 where C is the number of components
     component_counter = 0
+    # create a list of nodes using the neighbour_list
     node_list = [index for index, neighbour in enumerate(neighbour_list)] 
+    # create a label_list initialised to None for each node
     label_list = [None for node in node_list]
-    # replace None with subgraph label (subgraph label is from 0 to C-1, where C is the number of components)
+    
+    # loop through the label_list checking for None
     while None in label_list:
-        node = label_list.index(None)
-        visited_list = depth_first_search(neighbour_list, node)
+
+        # get a reference node for the first None in label_list
+        root_node = label_list.index(None)
+        # apply depth_first_search to the node
+        visited_list = depth_first_search(neighbour_list, root_node)
+
+        # label nodes according to their subgraph 
         for value in visited_list:
             label_list[value] = component_counter
+
+        # increment the component count
         component_counter = component_counter + 1
+
     return label_list
 
 ## Implementing this function is task 3:
@@ -151,23 +177,37 @@ def get_component_density(label, neighbour_list, label_list):
     are no nodes with the given component label, the function
     should raise an error.'''
 
+    # Depth First Search
     def depth_first_search(neighbour_list, root_node):
+        
         visited_nodes = set()
         stack = [root_node]
+
+        # create a path count 
         path_count = 0
+
         while stack:
+
             node = stack.pop()
+
             if node not in visited_nodes:
                 visited_nodes.add(node)
+                # find the unvisited neighbours of the rood node
                 resultant_neighbours = set(neighbour_list[node]) - visited_nodes
+                # increment the path count by the number of jumps between node and neighbours
                 path_count = path_count + len(resultant_neighbours)
                 stack.extend(resultant_neighbours)
+
+        # return the path_count variable
         return path_count
 
+    # find the first node in the label_list for the required label 
     root_node = label_list.index(label)
+    # determine the number of paths in the subgraph
     paths = depth_first_search(neighbour_list, root_node)
+    # determine the number of nodes in the subgraph
     nodes = label_list.count(label)
-
+    
     return paths/nodes
 
     # we can also get the full number of paths in the supergraph by collpasing the duplicates in the label_list and then iterating this function on the collapsed label_list
